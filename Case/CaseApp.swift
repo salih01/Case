904dc753d9983ae57e9bdc496680cf1c69a9a91e
Salih Çakmak <slhckmk0@gmail.com>
2022-05-13 20:13:33 +0300

    Initial Commit

---
+++ Case/CaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseApp.swift
+//  Case
+//
+//  Created by Salih Çakmak on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Case/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Case
+//
+//  Created by Salih Çakmak on 13.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
